443c447b891edd6b83f5f4e73c7d21a5e53f3e96
Maria V <tirimaria@gmail.com>
2024-11-21 13:03:10 +0000

    Fetch data every 15 seconds, store in dirs

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 A simple script to fetch and aggregate Vatsim flights data that is runned by Cron once a day.
+
+References:
+
+1. https://www.geeksforgeeks.org/create-a-directory-in-python/
+2. https://www.programiz.com/python-programming/string-interpolation
+3. https://realpython.com/python-sleep/
--- vatsim_data.py
+++ vatsim_data.py
@@ -1,35 +1,60 @@
 import requests
 import json
-import sqlite3
-from datetime import datetime
+import os
+import time
+from datetime import datetime, UTC
 
 def fetch_data():
-  r = requests.get("https://data.vatsim.net/v3/vatsim-data.json")
+    r = requests.get("https://data.vatsim.net/v3/vatsim-data.json")
+    if r.status_code == 200:
+        return r.json()
+    else:
+        print(f"Failed to fetch data. Status code: {r.status_code}")
+        return None
 
-  if r.status_code == 200:
-   return r.json()
-  else:
-    print(f"Failed to fetch data. Status code: {r.status_code}")
+def create_directory(directory_name): # References: [1]
+    try:
+        os.makedirs(directory_name, exist_ok=True)
+        print(f"Directory '{directory_name}' is ready.")
+    except PermissionError:
+        print(f"Permission denied: Unable to create '{directory_name}'.")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+    return os.path.abspath(directory_name)
+
+def retrieve():
+    data = fetch_data()
+    if data is None:
+        return
+
+    created_at = datetime.now(UTC).date().isoformat()
+    dir_name = create_directory(created_at)
+
+    if dir_name is None:
+        print("Directory creation failed. Exiting.")
+        return
+
+    # using the "update" timestamp from the data for the file name
+    last_timestamp = int(data["general"]["update"])
+    file_name_full = f'{last_timestamp}.json'  # References: [2]
+    file_path = os.path.join(dir_name, file_name_full)
+
+    with open(file_path, "w") as f:
+        json.dump(data, f)
+
+    print(f"Data written to: {file_path}")
+
+retrieve()
 
 
-def store_data(data):
-  conn = sqlite3.connect("vatsim_data.db")
-  cursor = conn.cursor()
+def main():
+    while True:
+      print('retrieve')
+      retrieve()
+      print('sleeping')
+      time.sleep(15)  # References: [3]
 
-  cursor.execute("""
-      CREATE TABLE IF NOT EXISTS daily_data (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          fetch_date TEXT UNIQUE,
-          data TEXT
-      )
-    """)
-
-  fetch_date = datetime.now().strftime("%Y-%m-%d")
-  cursor.execute("INSERT INTO daily_data (fetch_date, data) VALUES (?, ?)", (fetch_date, json.dumps(data)))
-
-  conn.commit()
-  conn.close()
-  print(f"Data stored in database for {fetch_date}")
-
-data = fetch_data()
-store_data(data)
+main()
