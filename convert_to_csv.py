213564137ad818aff11ab60e8d1316e1f8638d8a
Maria V <tirimaria@gmail.com>
2024-12-16 21:58:34 +0000

    Refactor CSV script

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -6,7 +6,6 @@ import csv
 def readdir(name):
   filenames = os.listdir(name)
   filenames.sort(key=sort_key)
-  print(filenames)
   for filename in filenames:
     full_path = os.path.join(name, filename)
     with open(full_path, "rb") as file:
@@ -19,36 +18,38 @@ def readdir(name):
 def sort_key(filename):
   return int(filename.split(".")[0])
 
-def prepare_pilots_csv():
-  with open('pilots.csv', 'w', newline='') as csvfile:
-    fieldnames = ['update', 'cid', 'name', 'callsign', 'latitude', 'aircraft_short', 'departure', 'arrival', 'enroute_time']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
+def prepare_csv():
+  for dir_name in os.listdir("."):
+    if dir_name.startswith("2024"):
 
-    for item in readdir("vatsim_data_debug_chunk"):
-      for pilot in item["pilots"]:
-        row = {'update': item["general"]["update"], 'cid': pilot["cid"],'name': pilot["name"], 'callsign': pilot["callsign"]}
+      controllers = open("controllers.csv", "w", newline='')
+      controllers_fieldnames = ['update', 'cid', 'name', 'callsign']
+      controllers_writer = csv.DictWriter(controllers, fieldnames=controllers_fieldnames)
 
-        # handling missing or inconstent data
-        row['latitude'] = (pilot["latitude"] or {})
-        row['departure'] = (pilot["flight_plan"] or {}).get("departure")
-        row["aircraft_short"] = (pilot["flight_plan"] or {}).get("aircraft_short")
-        row["arrival"] = (pilot["flight_plan"] or {}).get("arrival")
-        row["enroute_time"] = (pilot["flight_plan"] or {}).get("enroute_time")
-        writer.writerow(row)
+      pilots = open("pilots.csv", "w", newline='')
+      pilots_fieldnames = ['update', 'cid', 'name', 'callsign', 'latitude', 'longitude', 'aircraft_short', 'departure', 'arrival', 'enroute_time']
+      pilots_writer = csv.DictWriter(pilots, fieldnames=pilots_fieldnames)
 
+      controllers_writer.writeheader()
+      pilots_writer.writeheader()
 
-def prepare_controllers_csv():
-  with open('controllers.csv', 'w', newline='') as csvfile:
-    fieldnames = ['update', 'cid', 'name', 'callsign']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
+      for item in readdir(dir_name):
+        for controller in item["controllers"]:
+          row = {'update': item["general"]["update"], 'cid': controller["cid"],'name': controller["name"], 'callsign': controller["callsign"]}
+          controllers_writer.writerow(row)
 
-    for item in readdir("vatsim_data_debug_chunk"):
-      for controller in item["controllers"]:
-        row = {'update': item["general"]["update"], 'cid': controller["cid"],'name': controller["name"], 'callsign': controller["callsign"]}
-        writer.writerow(row)
+        for pilot in item["pilots"]:
+          row = {'update': item["general"]["update"], 'cid': pilot["cid"],'name': pilot["name"], 'callsign': pilot["callsign"]}
+
+          # handling missing or inconstent data
+          row['latitude'] = pilot["latitude"]
+          row['longitude'] = pilot["longitude"]
+          row['departure'] = (pilot["flight_plan"] or {}).get("departure")
+          row["aircraft_short"] = (pilot["flight_plan"] or {}).get("aircraft_short")
+          row["arrival"] = (pilot["flight_plan"] or {}).get("arrival")
+          row["enroute_time"] = (pilot["flight_plan"] or {}).get("enroute_time")
+          pilots_writer.writerow(row)
+
 
 if __name__ == "__main__":
-  prepare_pilots_csv()
-  prepare_controllers_csv()
+  prepare_csv()
